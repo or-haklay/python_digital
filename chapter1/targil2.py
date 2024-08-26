a35be53709f77f49a1f0d10f7add976557a14ccb
or-haklay <or.haklay@gmail.com>
2024-08-26 10:59:02 +0300

    second exercise on python

--- chapter1/targil2.py
+++ chapter1/targil2.py
@@ -4,21 +4,22 @@
 print("\nwellcome to are store \n\n our prices: \n tomato = 3 NIS per kg\n cucamber = 2 NIS per kg \n cola = 5 NIS \n chicken = 20 NIS per kg \n")
 
 #what you order
-tamato = int(input("how many tomatos do you want (kg)?\n"))
-cucmber = int(input("how many cucambes do you want (kg)?\n"))
-cola = int(input("how many colas do you want ?\n"))
-chicken = int( input("how many chickens do you want (kg)?\n"))
-
-print("your total is: " + str(int(1.17*(tamato+cucmber+cola+chicken)//1)) + " NIS")
+tomato =   3*int(input("how many tomatos do you want (kg)?\n"))
+cucmber =  2*int(input("how many cucambes do you want (kg)?\n"))
+cola = 5*int(input("how many colas do you want ?\n"))
+chicken = 20*int( input("how many chickens do you want (kg)?\n"))
 
 
 
 
+print("\nyour summery\n------------")
+print("tomato: " + str(tomato))
+print("cucmber: " + str(cucmber))
+print("cola: " + str(cola))
+print("chicken: " + str(chicken))
 
-
-
-
-
+print("\nyour total is: " + str(int((tomato+cucmber+cola+chicken)//1)) + " NIS without tax")
+print("your total is: " + str("%.1f"%(1.17*(tomato+cucmber+cola+chicken)//1)) + " NIS with tax")
 
 
 
